1b901b0a9cd671ba39de359c51a6c45020d154f5
Stewart Worrall <stewart.worrall@sydney.edu.au>
2023-02-20 12:05:47 +1100

    changes to the example asm project

--- example-project/Src/assembly.s
+++ example-project/Src/assembly.s
@@ -22,12 +22,22 @@ assembly_function:
 	LDR R2, =byte_array
 	LDR R4, =0x00
 
+	ADD R0, R1
+
 forever_loop:
 
 	LDRB R3, [R2, R4]
+	ADD R3, #0x01
+	STRB R3, [R2, R4]
 
-	ADD R0, R1
 	ADD R4, 0x01
+
+	@ questions - what happens when we go past the end of the array?
+	@  	        - what happens in c?
+
+	@ change the value of the addition
+	@ change the LDRB to LDRH (same for STR) -  what does this mean
+	@ 			 make sure to change the increment pointer - what happens otherwise
 
 
 	B forever_loop
